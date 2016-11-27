b27755d33d86febff4c0edeb9923e692ea6698a7
EricRibeiro <EricRibeiro@Outlook.com.br>
2016-11-27 15:18:07 -0200

    simple validators and a list for blood types were implemented

--- js/userInput.js
+++ js/userInput.js
@@ -1,82 +1,94 @@
-function userInput () {
-	var isValid = validateForm();
-	if ( isValid == false ) {
-		swal(
-		  'Campo em Branco',
-		  'Todos os campos são obrigatórios',
-		  'error'
-		)
-		event.preventDefault();
-	} else {
-		var userName = document.getElementById('name').value;
-		var userEmail = document.getElementById('email').value;
-		var userPhone = document.getElementById('phone').value;
-		var userLocation = document.getElementById('location').value;
-		var userBlood = document.getElementById('blood').value;
-		
-		// Array's declaration and values assignement
-		var userData = {}; 
-		userData.id = "";
-		userData.userName = userName;
-		userData.userEmail = userEmail;
-		userData.userPhone = userPhone;
-		userData.userLocation = userLocation;
-		userData.userBlood = userBlood;
-		
-		var sendData = JSON.stringify(userData);
-		
-		$.ajax({
-		    url:"https://api.myjson.com/bins",
-		    type:"POST",
-		    data: sendData,
-		    contentType:"application/json; charset=utf-8",
-		    dataType:"json",
-		    success: function(data, textStatus, jqXHR){
-		    	var userUri = data.uri;
-		    	userId = breakUri(userUri);
-		    	pushId(userData, userId);
-		    	swal({
-				  html: true,
-				  title: "Senha de Acesso: " + userId,
-				  text: "Guarde essa senha para entrar na sua sessão",
-				  type: "success",
-				  confirmButtonText: "Continuar",
-				  closeOnConfirm: false,
-				},    	  
-				  function() {
-				  	location.href = "../index.php"
-				 }
-				)
-		    }
-		});   
-	}
+function userInput() {
+    var isValid = validateForm();
+    var bloodValid = bloodCheck();
+    var emailValid = checkEmail();
+    if (isValid == false) {
+        swal('Campo em Branco', 'Todos os campos são obrigatórios', 'error')
+        event.preventDefault();
+    } else if (bloodValid == false) {
+        swal('Tipo Sanguíneo', 'Selecione seu tipo sanguíneo para prosseguir', 'error')
+        event.preventDefault();
+    } else if (emailValid == false) {
+        swal('Confirme seu Email', 'Os emails inseridos não são iguais', 'error')
+        event.preventDefault();
+    } else {
+        var userName = document.getElementById('name').value;
+        var userEmail = document.getElementById('email').value;
+        var userPhone = document.getElementById('phone').value;
+        var userLocation = document.getElementById('location').value;
+        var userBlood = document.getElementById('blood').value;
+        // Array's declaration and values assignement
+        var userData = {};
+        userData.id = "";
+        userData.userName = userName;
+        userData.userEmail = userEmail;
+        userData.userPhone = userPhone;
+        userData.userLocation = userLocation;
+        userData.userBlood = userBlood;
+        var sendData = JSON.stringify(userData);
+        $.ajax({
+            url: "https://api.myjson.com/bins",
+            type: "POST",
+            data: sendData,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(data, textStatus, jqXHR) {
+                var userUri = data.uri;
+                userId = breakUri(userUri);
+                pushId(userData, userId);
+                swal({
+                    html: true,
+                    title: "Senha de Acesso: " + userId,
+                    text: "Guarde essa senha para entrar na sua sessão",
+                    type: "success",
+                    confirmButtonText: "Continuar",
+                    closeOnConfirm: false,
+                }, function() {
+                    location.href = "../index.php"
+                })
+            }
+        });
+    }
 }
-
 // Function to break the URI and get the ID
 function breakUri(userUri) {
-	var splittedURL = userUri.split(/\/+/g);
-	var userId = splittedURL[splittedURL.length-1];
-	return userId
+    var splittedURL = userUri.split(/\/+/g);
+    var userId = splittedURL[splittedURL.length - 1];
+    return userId
 }
-
 //Function to update the object with the user's ID
 function pushId(userData, userId) {
-	userData.id = userId;
-	sendData = JSON.stringify(userData);
-	$.ajax({
-    url:"https://api.myjson.com/bins/" + userId,
-    type:"PUT",
-    data: sendData,
-    contentType:"application/json; charset=utf-8",
-    dataType:"json",
-});   
+    userData.id = userId;
+    sendData = JSON.stringify(userData);
+    $.ajax({
+        url: "https://api.myjson.com/bins/" + userId,
+        type: "PUT",
+        data: sendData,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+    });
+}
+//Functions to valdiate the form
+function validateForm() {
+    if (document.getElementsByTagName("input")[0].value == "" || document.getElementsByTagName("input")[1].value == "" || document.getElementsByTagName("input")[2].value == "" || document.getElementsByTagName("input")[3].value == "" || document.getElementsByTagName("input")[4].value == "") {
+        return false;
+    } else {
+        return true;
+    }
 }
 
-//Function to valdiate the form
-function validateForm() {
-	if (document.getElementsByTagName("input")[0].value == "" || document.getElementsByTagName("input")[1].value == "" || document.getElementsByTagName("input")[2].value == "" || document.getElementsByTagName("input")[3].value == "" || document.getElementsByTagName("input")[4].value == "") {
-		return false;
-	} else {
-		return true;
-	}
+function bloodCheck() {
+    if (document.getElementById("blood").value == "Tipo Sanguíneo") {
+        return false
+    } else {
+        return true
+    }
+}
+
+function checkEmail() {
+    if (document.getElementById('email').value == document.getElementById('emailConfirm').value) {
+        return true
+    } else {
+        return false
+    }
 }
